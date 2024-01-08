6762c459fb310258583ebc70f1523cee28d63a49
Matteo Pierro <pierromatteo@gmail.com>
2024-01-08 22:05:11 +0200

    it farted but i shall still commit

--- src/main.rs
+++ src/main.rs
@@ -23,13 +23,42 @@ mod captcha_test {
     use crate::{evaluate_pair, solve_captcha};
 
     #[test]
-    fn test4() {
-        let result = evaluate_pair(1, 1);
-        assert_eq!(result, 1);
+    fn test_evaluate_pair() {
+        assert_eq!(evaluate_pair(1, 1), 1);
     }
 
     #[test]
-    fn test5() {
+    fn test_windowing() {
+        assert_eq!(window_vector(vec![1, 1, 1]), vec![(1, 1), (1, 1)]);
+        assert_eq!(window_vector(vec![1, 2, 3]), vec![(1, 2), (2, 3)]);
+    }
+
+    #[test]
+    fn test_circular_windowing() {
+        assert_eq!(circular_windowing(vec![]), vec![]);
+        assert_eq!(circular_windowing(vec![1, 1]), vec![(1, 1), (1, 1)]);
+        assert_eq!(circular_windowing(vec![1, 2]), vec![(1, 2), (2, 1)]);
+    }
+
+    fn circular_windowing(input: Vec<i32>) -> Vec<(i32, i32)> {
+        if input.len() == 0 {
+            return vec![];
+        }
+        let mut circular_input = input.clone();
+        circular_input.push(*input.first().unwrap());
+        return window_vector(circular_input)
+    }
+
+    fn window_vector(vector: Vec<i32>) -> Vec<(i32, i32)> {
+        let mut result = vec![];
+        for i in 0..vector.len()-1{
+            result.push((vector[i], vector[i+1]));
+        }
+        result
+    }
+
+    #[test]
+    fn test_mismatching_pair_evaluates_to_zero() {
         let result = evaluate_pair(1, 2);
         assert_eq!(result, 0);
     }
