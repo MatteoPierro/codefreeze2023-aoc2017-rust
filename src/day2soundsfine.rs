1bcea46df7d476bdf5d6b73073195ff04798985d
Matteo Pierro <pierromatteo@gmail.com>
2024-01-09 22:35:05 +0200

    parse file lines

--- src/day2soundsfine.rs
+++ src/day2soundsfine.rs
@@ -10,6 +10,7 @@ fn min_value(input: &Vec<i32>) -> i32 {
 mod checksum_corruption_test {
     use crate::*;
     use crate::day2soundsfine::{max_value, min_value};
+    use crate::input_reader::{read_input_file, read_lines};
 
     #[test]
     fn test_max_value() {
@@ -17,7 +18,7 @@ mod checksum_corruption_test {
     }
     #[test]
     fn test_calculate_checksum() {
-        assert_eq!(8, calculate_checksum(&vec![5,1,9,5]));
+        assert_eq!(8, calculate_row_checksum(&vec![5, 1, 9, 5]));
     }
 
     #[test]
@@ -31,16 +32,35 @@ mod checksum_corruption_test {
         assert_eq!(vec![1, 2, 3], parse_line("1\t2\t3"))
     }
 
+    #[test]
+    fn test_parse_lines() {
+        assert_eq!(vec![vec![1],vec![1]], parse_lines(vec!["1", "1"]));
+        assert_eq!(vec![vec![1,2,3],vec![4,5,6]], parse_lines(vec!["1\t2\t3", "4\t5\t6"]))
+    }
+
+    #[test]
+    fn test_full_checksum() {
+        let lines = read_lines(&read_input_file("day2.txt"));
+        lines.iter().map(|line| parse_line(line)).collect::<Vec<Vec<i32>>>();
+
+        assert_eq!(0,0)
+    }
+
+    fn parse_lines(input: Vec<&str>) -> Vec<Vec<i32>> {
+        input.iter().map(|x| parse_line(x)).collect()
+    }
+
     fn parse_line(input: &str) -> Vec<i32> {
-        let mut cell_iterator = input.split_whitespace();
-        cell_iterator.map(|cell| cell.parse::<i32>().unwrap()).collect()
+        input.split_whitespace()
+            .map(|cell| cell.parse::<i32>().unwrap())
+            .collect()
     }
 
     fn sum_rows_checksum(input: Vec<Vec<i32>>) -> i32 {
-        input.iter().map(|row| calculate_checksum(row)).sum()
+        input.iter().map(|row| calculate_row_checksum(row)).sum()
     }
 
-    fn calculate_checksum(input: &Vec<i32>) -> i32 {
+    fn calculate_row_checksum(input: &Vec<i32>) -> i32 {
         max_value(&input) - min_value(&input)
     }
 }
