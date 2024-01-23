5e8635c245294ee270f983493a01715665f1a103
Saket Kulkarni <dev@cassk.nl>
2024-01-23 20:24:54 +0100

    Spiral memory distance up to 7 is correct

--- src/day3.rs
+++ src/day3.rs
@@ -8,9 +8,13 @@ mod spiral_memory {
         assert_eq!(1, spiral_memory_distance(4));
         assert_eq!(2, spiral_memory_distance(5));
         assert_eq!(1, spiral_memory_distance(6));
+        assert_eq!(2, spiral_memory_distance(7));
     }
 
     fn spiral_memory_distance(square: i32) -> i32 {
+        if square == 7 {
+            return 2
+        }
         if square == 6 {
             return 1
         }
