7f06bca48d47b476df48f9c349a3a08473cb36e7
Saket Kulkarni <dev@cassk.nl>
2024-01-23 20:13:39 +0100

    Format and rename test

--- src/day3.rs
+++ src/day3.rs
@@ -1,10 +1,8 @@
 #[cfg(test)]
 mod spiral_memory {
-
     #[test]
-    fn test_max_value() {
-        
-        assert_eq!(0,spiral_memory_distance(1));
+    fn test_spiral_memory_distance() {
+        assert_eq!(0, spiral_memory_distance(1));
     }
 
     fn spiral_memory_distance(square: i32) -> i32 {
