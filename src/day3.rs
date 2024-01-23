4be65ce97d4ea36cf8fa004d70193c6e4b5bf960
Saket Kulkarni <dev@cassk.nl>
2024-01-23 20:17:51 +0100

    Spiral memory distance of 2 is 1

--- src/day3.rs
+++ src/day3.rs
@@ -3,9 +3,10 @@ mod spiral_memory {
     #[test]
     fn test_spiral_memory_distance() {
         assert_eq!(0, spiral_memory_distance(1));
+        assert_eq!(1, spiral_memory_distance(2));
     }
 
     fn spiral_memory_distance(square: i32) -> i32 {
-        0
+        square - 1
     }
 }
